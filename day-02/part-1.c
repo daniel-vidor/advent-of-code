4c57b90f10276b5efd86d81a8464a08b1501d58d
Daniel Vidor <dvidor@proton.me>
2023-12-13 00:28:23 +0000

    Add string processing functions

--- day-02/part-1.c
+++ day-02/part-1.c
@@ -11,26 +11,45 @@ const int MAX_GREEN_CUBES = 13;
 const int MAX_BLUE_CUBES = 14;
 
 FILE* open_input_file(const char* filename);
+int strsplit(char* string, const char* delim, char** split_string);
 char* strsep(char **stringp, const char *delim);
-void print_string_array(char** str_arr);
-char** split(char* string, const char* delim);
+void print_string_array(char** str_arr, int length);
 
 int main() {
-    char c;
+    int line_number = 1;
+    int token_count;
     char* found;
-    FILE* file;
-    file = open_input_file(INPUT_FILE);
 
     char line[MAX_LINE_SIZE];
-    char** str_arr_colon;
+    char* split_string[100];
+
+    FILE* file = open_input_file(INPUT_FILE);
     while (fgets(line, sizeof(line), file) != NULL) {
-        printf("%s", line);
-        str_arr_colon = split(line, ":");
-        print_string_array(str_arr_colon);
+        printf("Reading line %d...\n", line_number++);
+
+        line[strcspn(line, "\n")] = '\0'; // Strip off newline
+        printf("\t\"%s\"\n", line);
+
+        token_count = strsplit(line, ":", split_string);
+        print_string_array(split_string, token_count);
+
+        token_count = strsplit(split_string[1], ";", split_string);
+        print_string_array(split_string, token_count);
+
+        char* split_string_sub[100];
+        int token_count_sub = 0;
+        for (int i = 0; i < token_count; i++) {
+            token_count_sub = strsplit(split_string[i], ",", split_string_sub);
+            // token_count_sub = strsplit(split_string[i], ",", split_string_sub[token_count_sub]);
+        }
+        print_string_array(split_string_sub, token_count_sub);
+
+        putchar('\n');
     }
 
     return 0;
 }
+
 
 FILE* open_input_file(const char* filename) {
     FILE *file;
@@ -44,35 +63,40 @@ FILE* open_input_file(const char* filename) {
     return file;
 }
 
-char** split(char* string, const char* delim) {
-    string[strcspn(string, "\n")] = '\0'; // Strip off newline
-    printf("Splitting \"%s\" by '%s'\n", string, delim);
+/**
+ * Splits a given string, `string`, by a given delimiter, `delim`, and outputs the result to `split_string`.
+ * Returns the number of split elements.
+ */
+int strsplit(char* string, const char* delim, char** split_string) {
+    printf("Splitting \"%s\" by '%s'...\n", string, delim);
 
-    char* split_string[50];
     int count = 0;
-    char* copied_string;
-    strcpy(copied_string, string);
     char* token;
     
-    while (token = strsep(&copied_string, delim)) {
-        printf(token);
-        // split_string[count++] = token;
-        strcpy(split_string[count++], token);
+    while (token = strsep(&string, delim)) {
+        printf("\tToken: \"%s\"\n", token);
+        split_string[count++] = token;
     }
 
-    return &split_string;
+    return count;
 }
 
-void print_string_array(char** str_arr) {
+/** Prints an array of strings. */
+void print_string_array(char** str_arr, int length) {
     printf("Printing string array...\n");
-    int length = (sizeof str_arr) / (sizeof str_arr[0]);
-    for (int i = 0; i < length; i++)
-        printf("[%s], ", str_arr[i]);
+    printf("\t[");
 
-    putchar('\n');
+    for (int i = 0; i < length; i++) {
+        printf("\"%s\"", str_arr[i]);
+
+        if (i < length - 1)
+            printf(", ");
+    }
+
+    printf("]\n");
 }
 
-/** strsep is not in MinGW's environment, so it has been copied here. */
+/** `strsep` is not in MinGW's environment, so it has been copied here. */
 char *strsep(char **stringp, const char *delim) {
     char *rv = *stringp;
     if (rv) {
